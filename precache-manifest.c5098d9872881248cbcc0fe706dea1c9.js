2505747e6a9409db044cc6f79fa8e6cfd7c34f49
Adesh-13 <adeshnewaskar@gmail.com>
2020-08-30 12:43:27 +0530

    Updates

---
+++ precache-manifest.c5098d9872881248cbcc0fe706dea1c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6be2a183c607a3d40c64ae19ea8a723",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "8b9175b2f001e3133f39",
+    "url": "/Netflix-clone/static/css/main.1040ce49.chunk.css"
+  },
+  {
+    "revision": "4c701943a2c15051c75b",
+    "url": "/Netflix-clone/static/js/2.bf6e9201.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-clone/static/js/2.bf6e9201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b9175b2f001e3133f39",
+    "url": "/Netflix-clone/static/js/main.fbd3dc6c.chunk.js"
+  },
+  {
+    "revision": "1f70fcb38d92599a6c08",
+    "url": "/Netflix-clone/static/js/runtime-main.d8bb3d81.js"
+  }
+]);
